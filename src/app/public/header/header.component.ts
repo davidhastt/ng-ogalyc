70c6ad348688820f3b52f54a92a88245c1b8bb05
david <davidhastt@gmail.com>
2026-05-08 09:59:02 -0600

    La galeria de ediciones esta lista pero no tiene links

--- src/app/public/header/header.component.ts
+++ src/app/public/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { DatePipe } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-header',
-  imports: [DatePipe],
+  imports: [DatePipe, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
